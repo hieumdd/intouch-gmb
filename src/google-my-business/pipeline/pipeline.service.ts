3822100b7dd8567cf2b7d3462a8babc919c56215
HM <hieumdd@gmail.com>
2023-05-12 23:14:46 +0700

    feat: Add more accounts

--- src/google-my-business/pipeline/pipeline.service.ts
+++ src/google-my-business/pipeline/pipeline.service.ts
@@ -7,6 +7,42 @@ import { getInsights } from '../insight/insight.service';
 import { getReviews } from '../review/review.service';
 import { locationSchema, insightSchema, reviewSchema } from './pipeline.schema';
 
+export const ACCOUNT_IDS = [
+    '108410633950303010387',
+    '110318830513658384132',
+    '106229460503813799776',
+    '100573566092104440301',
+    '102557082445185740080',
+    '113525279692964740608',
+    '116319944937011819347',
+    '116102622521931142940',
+    '109564451008494963787',
+    '108377208689430486449',
+    '105349743222273578941',
+    '117711380718861870899',
+    '118431062178951153484',
+    '115347226326702644007',
+    '109713237601553973412',
+    '106289254611160713633',
+    '110330971716857283417',
+    '113474757162097686232',
+    '114773234823613337140',
+    '109031214894962719165',
+    '108162336638149981189',
+    '104902727633726943636',
+    '114540406156996221549',
+    '111247570102565088769',
+    '115095552779082091769',
+    '118297400137675290332',
+    '100257412545533878399',
+    '117245270700982853551',
+    '108405109682017952426',
+    '101789019625300647506',
+    '112149756678977647532',
+    '108071160239924318608',
+    '117258147294478852396',
+    '116471768159485451813',
+];
 export type RunPipelinesOptions = {
     start: string;
     end: string;
@@ -37,15 +73,12 @@ export const runPipelines = async ({ start, end }: RunPipelinesOptions) => {
             load(insights, { table: `Insight__${accountId}`, schema: insightSchema }),
             load(reviews, { table: `Review__${accountId}`, schema: reviewSchema }),
         ]).then(() => ({
+            accountId,
             location: locations.length,
             insight: insights.length,
             review: reviews.length,
         }));
     };
 
-    return Promise.all(
-        ['108405109682017952426', '107962161788712140767'].map((accountId) => {
-            return runPipeline(accountId);
-        }),
-    );
+    return Promise.all(ACCOUNT_IDS.map((accountId) => runPipeline(accountId)));
 };
