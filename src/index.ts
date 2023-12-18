e69eb147e016159d8bc5652b87b3d6695e1e833b
HM <hieumdd@gmail.com>
2023-12-18 21:00:24 +0700

    feat: Add Joi validators

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { ValidatedRequest, createValidator } from 'express-joi-validation';
 
 import { logger } from './logging.service';
 import { exchangeCodeForToken, getAuthorizationURL } from './google-my-business/auth/auth.service';
-import { CallbackQuerySchema } from './google-my-business/auth/auth.request.dto';
+import { CallbackQueryRequest, CallbackQuerySchema } from './google-my-business/auth/auth.request.dto';
 import * as pipelines from './pipeline/pipeline.const';
 import { initiatePipelines, runLocationPipeline } from './pipeline/pipeline.service';
-import { RunLocationPipelineBodySchema } from './pipeline/pipeline.request.dto';
+import { RunLocationPipelineRequest, RunLocationPipelineBodySchema } from './pipeline/pipeline.request.dto';
 
 ['SIGINT', 'SIGTERM'].forEach((signal) => {
     process.on(signal, () => {
@@ -16,16 +17,15 @@ import { RunLocationPipelineBodySchema } from './pipeline/pipeline.request.dto';
 });
 
 const app = express();
+const validator = createValidator({ joi: { stripUnknown: true } });
 
 app.use(bodyParser.json());
 
 app.use(({ method, path, body }, res, next) => {
     logger.info({ method, path, body });
-
     res.on('finish', () => {
         logger.info({ method, path, body, status: res.statusCode });
     });
-
     next();
 });
 
@@ -33,32 +33,31 @@ app.get('/authorize', (_, res) => {
     res.status(301).redirect(getAuthorizationURL());
 });
 
-app.get('/authorize/callback', ({ query }, res) => {
-    CallbackQuerySchema.validateAsync(query, { stripUnknown: true })
-        .then(({ code }) => {
-            exchangeCodeForToken(code).then((token) => res.status(200).json({ token }));
-        })
-        .catch((error) => {
-            logger.warn({ error });
-            res.status(400).json({ error });
-        });
-});
+app.get(
+    '/authorize/callback',
+    validator.query(CallbackQuerySchema),
+    ({ query: { code } }: ValidatedRequest<CallbackQueryRequest>, res) => {
+        exchangeCodeForToken(code)
+            .then((token) => res.status(200).json({ token }))
+            .catch((error) => {
+                logger.error({ error });
+                res.status(500).json({ error });
+            });
+    },
+);
 
-app.post(`/${pipelines.Location.route}`, ({ body }, res) => {
-    RunLocationPipelineBodySchema.validateAsync(body)
-        .then((options) => {
-            runLocationPipeline(options)
-                .then((result) => res.status(200).json({ result }))
-                .catch((error) => {
-                    logger.error({ error });
-                    res.status(500).json({ error });
-                });
-        })
-        .catch((error) => {
-            logger.warn({ error });
-            res.status(400).json({ error });
-        });
-});
+app.post(
+    `/${pipelines.Location.route}`,
+    validator.body(RunLocationPipelineBodySchema),
+    ({ body }: ValidatedRequest<RunLocationPipelineRequest>, res) => {
+        runLocationPipeline(body)
+            .then((result) => res.status(200).json({ result }))
+            .catch((error) => {
+                logger.error({ error });
+                res.status(500).json({ error });
+            });
+    },
+);
 
 app.post(`/`, (_, res) => {
     initiatePipelines()
--- src/pipeline/pipeline.request.dto.ts
+++ src/pipeline/pipeline.request.dto.ts
@@ -1,8 +1,15 @@
 import Joi from 'joi';
+import { ContainerTypes, ValidatedRequestSchema } from 'express-joi-validation';
 
 import { RunLocationPipelineOptions } from './pipeline.service';
 
-export const RunLocationPipelineBodySchema = Joi.object<RunLocationPipelineOptions>({
+export type RunLocationPipelineBody = RunLocationPipelineOptions;
+
+export interface RunLocationPipelineRequest extends ValidatedRequestSchema {
+    [ContainerTypes.Body]: RunLocationPipelineBody;
+}
+
+export const RunLocationPipelineBodySchema = Joi.object<RunLocationPipelineBody>({
     businessId: Joi.string().required(),
     accountId: Joi.string().required(),
     locationId: Joi.string().required(),
